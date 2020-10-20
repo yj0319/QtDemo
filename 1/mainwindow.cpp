1976089220d8c725e87a9df0c6819160326dc2d2
yj0319 <youjie9658@qq.com>
2020-10-20 21:39:16 +0800

    加了一个按钮

--- 1/mainwindow.cpp
+++ 1/mainwindow.cpp
@@ -1,11 +1,12 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include<QDebug>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+   qDebug()<<"你好呀";
 }
 
 MainWindow::~MainWindow()
